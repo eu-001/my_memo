f868dcd17c4371de54b51d7b779ad64da7ac0c18
eu-001 <chcolatei71@gmail.com>
2025-07-31 13:21:06 +0900

    00

---
+++ src/pages/test1.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuthStore } from "../store/authStore";
+import { useNavigate } from "react-router-dom";
+
+const test1: React.FC = () => {
+  
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default test1;
