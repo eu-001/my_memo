d438f2eca7e175b0af55f50a41a3511ed2ac2300
eu-001 <chcolatei71@gmail.com>
2025-07-31 15:16:53 +0900

    00

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -4,13 +4,18 @@ import { useAuthStore } from "../store/authStore";
 import { useNavigate } from "react-router-dom";
 
 const Test1: React.FC = () => {
+  const fetchData=async () => {
+    try{
+      let response: any = await axios
+        .get("http://localhost:3001/api/test1")
+      console.log(`## response:`, Response)
+      response = response?.data
+      console.log(`## response.data:`, Response)
+    } catch (error:any) {
+
+    }
+  }
   useEffect(()=>{
-    axios
-      .get("http://localhost:3001/api/test1")
-      .then((Response: any) => {
-        console.log(`## response:`, Response)
-      })
-      .catch((err: any) => {})
   },[]);
 
   return (
